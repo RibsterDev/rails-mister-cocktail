bc0b3f46290dbfe544633b0ebd4785eabffdd058
jeremy gibilaro <jeremygibilaro@gmail.com>
2019-11-07 15:04:32 +0100

    rake almost done part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :cocktails
+    # resources :cocktails do
+    # resources :doses, only: [:create, :new]
   resources :doses
   resources :ingredients
-  resources :cocktails
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
