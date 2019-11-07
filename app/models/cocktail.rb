2f89b8e52e8223d777b1c6d3d6d7d2e788b537b1
jeremy gibilaro <jeremygibilaro@gmail.com>
2019-11-07 17:54:14 +0100

    ça avance bien

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,10 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+
   validates :name, presence: true, uniqueness: true
+
+  def image_url
+    Unsplash::Photo.search(name)[0]['urls']['regular']
+  end
 end
